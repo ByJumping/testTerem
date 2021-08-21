a919ba257af1a20f4097a79a2047a21e8313ca7a
Aleksey Pravdin <leks16@bk.ru>
2021-08-21 23:20:11 +0300

    доделан метод запроса на сервер

--- 2/script.js
+++ 2/script.js
@@ -1,50 +1,50 @@
-const submit = document.querySelector('.submit')
-const data = document.querySelector('.data')
-const getBtn = document.querySelector('.get')
-const select = document.querySelector('.select')
-const input1 = document.querySelector('.input1')
-const input2 = document.querySelector('.input2')
-
-
+const submit = document.querySelector('.submit');
+const data = document.querySelector('.data');
+const getBtn = document.querySelector('.get');
+const select = document.querySelector('.select');
+const input1 = document.querySelector('.input1');
+const input2 = document.querySelector('.input2');
+const people = [];
 
 submit.addEventListener('click', (e)=>{
-    e.preventDefault()
+    e.preventDefault();
     if(input1.value === '' || input2.value === '') {
         alert('Ошибка, введите данные')
-    }else {
+    } else {
         data.insertAdjacentHTML('beforeend', `
-    <div>Select: ${select.value}</div>
-    <div>Первое значение: ${input1.value}</div>
-    <div>Второе значение: ${input2.value}</div>
-    `)
-        createPerson().then()
+            <div>Select: ${select.value}</div>
+            <div>Первое значение: ${input1.value}</div>
+            <div>Второе значение: ${input2.value}</div>
+        `);
+        createPerson().then();
     }
-})
+});
 
 getBtn.addEventListener('click', ()=> {
     sendRequest('GET', 'https://terem-1c850-default-rtdb.firebaseio.com/people.json')
-        .then(data=> console.log(Object.keys(data).map(key=> {
-            return {
-                ...data[key]
-            }
-        })))
-        .catch(err=> console.log(err))
+        .then(data=> console.log(Object.keys(data).map(key => data[key])))
+        .catch(err=> console.log(err));
 })
 
-
 function sendRequest(method, url, body = null) {
-    return fetch(url).then(response=> {
-        return response.json()
-    })
+    return fetch(url).then(response => response.json());
 }
-sendRequest('GET', 'https://terem-1c850-default-rtdb.firebaseio.com/people.json')
-.then(data=> console.log(Object.keys(data).map(key=> {
-    return {
-        ...data[key]
-    }
-})))
-.catch(err=> console.log(err))
 
+sendRequest('GET', 'https://terem-1c850-default-rtdb.firebaseio.com/people.json')
+.then(data => {
+    const people = Object.keys(data).map(key=> data[key]);
+    return generateHtml(people);
+}).catch(err=> console.log(err));
+
+function generateHtml(data1) {
+    for (let item of data1) {
+        data.insertAdjacentHTML('beforeend', `
+            <div>Select: ${item.select}</div>
+            <div>Первое значение: ${item.firstName}</div>
+            <div>Второе значение: ${item.lastName}</div>
+        `);
+    }
+}
 
 function createPerson() {
     return fetch('https://terem-1c850-default-rtdb.firebaseio.com/people.json', {
@@ -59,3 +59,4 @@ function createPerson() {
         })
     })
 }
+
